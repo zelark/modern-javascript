61480f8986ab59659b791db6aca12cdc53f708da
Aleksandr Zhuravlev <kvark.lark@gmail.com>
2018-01-18 21:17:02 +0300

    Transpiling code for new language features (babel)

--- bundle.js
+++ bundle.js
@@ -16270,11 +16270,22 @@ return zhTw;
 /* 120 */
 /***/ (function(module, exports, __webpack_require__) {
 
-// index.js
-var moment = __webpack_require__(0)
+"use strict";
 
-console.log("Hello from JavaScript!");
-console.log(moment().startOf('day').fromNow());
+
+var _moment = __webpack_require__(0);
+
+var _moment2 = _interopRequireDefault(_moment);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+console.log("Hello from JavaScript!"); // index.js
+
+console.log((0, _moment2.default)().startOf('day').fromNow());
+
+var name = "Bob",
+    time = "today";
+console.log('Hello ' + name + ', how are you ' + time + '?');
 
 /***/ }),
 /* 121 */
--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 // index.js
-var moment = require('moment')
+import moment from 'moment';
 
 console.log("Hello from JavaScript!");
 console.log(moment().startOf('day').fromNow());
+
+var name = "Bob", time = "today";
+console.log(`Hello ${name}, how are you ${time}?`);
